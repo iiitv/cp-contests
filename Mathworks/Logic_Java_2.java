cdf811804c452e78b1e030bc5758aa977e509ada
TanmayAmbadkar <42721553+TanmayAmbadkar@users.noreply.github.com>
2018-12-18 17:09:52 +0530

    Create Logic_Java_2.java

---
+++ Mathworks/Logic_Java_2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.math.*;
+public class Question1_adv
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int t=0;
+        for(int i=0;i<t;i++)
+        {
+            BigInteger n=new BigInteger(in.nextLine());
+            BigInteger a=new BigInteger("2");
+            BigInteger b=new BigInteger("5");
+            BigInteger c=new BigInteger("7");
+            BigInteger d=new BigInteger("9");			
+            if((n.mod(a)==BigInteger.ZERO)&&(n.mod(b)==BigInteger.ZERO)&&(n.mod(c)==BigInteger.ZERO)&&(n.mod(d)==BigInteger.ZERO))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
