262c9edcc1a41b800b3d7b10ee65826acb6d5597
TanmayAmbadkar <42721553+TanmayAmbadkar@users.noreply.github.com>
2018-12-18 18:09:05 +0530

    Create Logic.c

---
+++ CashInThatCarry/Logic.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int t=0;
+	scanf("%d",&t);
+	int i=0;
+	for(i=0;i<t;i++)
+	{
+		int n1=0;
+		int n2=0;
+		char car[50]="";
+		scanf("%d",&n1);
+		scanf("%d",&n2);
+		int t2=n2;
+		int t1=n1;
+		int c=0;
+		int carry=0;
+		while(t2>0)
+        {
+            int y=t2%10;
+            carry=0;
+            t1=n1;
+            while(t1>0)
+            {
+				int x=t1%10;
+                int z=x*y+carry;
+                carry=z/10;
+                if(carry>0)
+                {
+                    car[c]=carry+'0';
+                    c++;
+                }
+                t1=t1/10;
+            }
+            t2=t2/10;
+        }
+		printf("%d %s \n",c,car);
+	}
+	return 0;
+}
