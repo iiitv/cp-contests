60b9470240f8f7837b75a2046368641cbb60d0f8
TanmayAmbadkar <42721553+TanmayAmbadkar@users.noreply.github.com>
2018-12-21 21:37:42 +0530

    Update logic.c

--- highRisen/logic.c
+++ highRisen/logic.c
@@ -2,22 +2,22 @@
 #include <math.h>
 
 int main(){
-		int T;
-		scanf("%d",&T);
-		while(T--){
-				int r,blocks;
-				long long n;
-				scanf("%d%lld",&r,&n);
-				blocks = floor( log(r)/log(4) ) - 1;
-				if( blocks == 1 || blocks == 2 ){
-					printf("NO SHIFTING\n");
-				}
-				else{
-					long long x, min_steps = 0;
-					x = floor( ( -1 + (sqrt( 8*n - 7 )) )/2 );
-					min_steps = 1 + floor( n - (x*(x - 1))/2 - 1)*pow(2,x);
-					printf("%lld\n",min_steps%(1000000007));
-				}
-		}
-		return 0;
+        int T;
+        scanf("%d",&T);
+        while(T--){
+                int r,blocks;
+                int n;
+                scanf("%d %d",&r,&n);
+                blocks = ( log(r)/log(4) ) - 1;
+                if( blocks <=2 ){
+                    printf("NO SHIFTING\n");
+                }
+                else{
+                    long x, min_steps = 0;
+                    x = ( ( -1 + (sqrt( 8*n - 7 )) )/2 );
+                    min_steps = 1 + ( n - (x*(x - 1))/2 - 1)*pow(2,x);
+                    printf("%ld\n",min_steps%(1000000007));
+                }
+        }
+        return 0;
 }
