778409177c9dbc399fa9df2bf4e45a649c9a9e23
TanmayAmbadkar <42721553+TanmayAmbadkar@users.noreply.github.com>
2018-12-21 22:26:26 +0530

    Update logic.java

--- bubbleBee/logic.java
+++ bubbleBee/logic.java
@@ -8,13 +8,14 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-         Scanner in=new Scanner(System.in);
-         int t=in.nextInt();
-         while(t>0)
-         {
-             int r=in.nextInt();
-             double ar=0.4194*r*r;
-             System.out.printf("%.2f\n",ar);
-         }
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t>0)
+        {
+            long c=in.nextInt();
+            long n=in.nextInt();
+            BigInteger r=BigInteger.valueOf(c*n*n*n).mod(BigInteger.valueOf(1000000007));
+            System.out.println(r);
+        }
     }
 }
